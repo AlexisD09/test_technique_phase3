ae23798cafae10c9b86a02d7e244f975c2a2f768
Denhez Alexis <denhez.alexis@gmail.com>
2025-11-25 10:49:59 +0100

    First algo, 1 sample

--- functions.js
+++ functions.js
@@ -45,4 +45,15 @@ function extractDataFromJson() {
     }
 }
 
-module.exports = { extractDataFromJson };
+function calculateEndTime(startTime, analysisTime) {
+    const [hours, minutes] = startTime.split(':').map(Number);
+
+    let totalMinutes = hours * 60 + minutes + analysisTime;
+
+    const newHours = Math.floor(totalMinutes / 60);
+    const newMinutes = totalMinutes % 60;
+
+    return `${String(newHours).padStart(2, '0')}:${String(newMinutes).padStart(2, '0')}`;
+}
+
+module.exports = { extractDataFromJson, calculateEndTime };
--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-const { extractDataFromJson } = require('./functions.js');
+const { extractDataFromJson, calculateEndTime } = require('./functions.js');
 
 function run() {
     let data = extractDataFromJson();
+    const samples = data.samples;
+    const technicians = data.technicians;
+    const equipments = data.equipments;
+    let schedule = [];
 
+    for(let i = 0; i < samples.length; i++) {
+        const sampleType = samples[i].type;
+        let assignedTechnician = 0;
+        let assignedEquipment = 0;
+        let endTime = 0;
+
+        for (let j = 0; j < technicians.length; j++) {
+            const speciality = technicians[j].speciality;
+
+            if(speciality === sampleType) {
+                assignedTechnician = technicians[j].id;
+            }
+        }
+
+        for (let j = 0; j < equipments.length; j++) {
+            const equipmentType = equipments[j].type;
+
+            if(equipmentType === sampleType) {
+                assignedEquipment = equipments[j].id;
+            }
+        }
+
+        if(assignedTechnician !== 0 && assignedEquipment !== 0) {
+            endTime = calculateEndTime(samples[i].arrivalTime, samples[i].analysisTime);
+        }
+
+        schedule.push({
+            sampleId: samples[i].id,
+            technicianId: assignedTechnician,
+            equipmentId: assignedEquipment,
+            startTime: samples[i].arrivalTime,
+            endTime: endTime,
+        })
+    }
+    
+    console.log(schedule);
 }
 
 run();
